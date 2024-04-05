93e329f1e43dc2bf7abc1d0d50295edd4f87b133
Andor Horvat <andorhorvat@gmail.com>
2024-04-05 15:04:41 +0200

    Debugbar installed.

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
 
-$x = 1;
+require __DIR__ . '/vendor/autoload.php';
 
-echo "Xdebug";
-echo phpinfo();
+use DebugBar\StandardDebugBar;
+
+$debugbar = new StandardDebugBar();
+$debugbarRenderer = $debugbar->getJavascriptRenderer();
+
+$debugbar["messages"]->addMessage("hello world!");
+?>
+
+
+<html>
+<head>
+    <?php echo $debugbarRenderer->renderHead() ?>
+</head>
+<body>
+    ...
+    <?php echo $debugbarRenderer->render() ?>
+</body>
+</html>
