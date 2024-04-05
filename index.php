e2c8a1748f3cb4b5158184b902ce9cd95d681286
Andor Horvat <andorhorvat@gmail.com>
2024-04-05 15:35:25 +0200

    Router added.

--- index.php
+++ index.php
@@ -1,16 +1,42 @@
 <?php
 
+// AUTOLOAD****************************************************
 require __DIR__ . '/vendor/autoload.php';
 
-use DebugBar\StandardDebugBar;
 
+
+// DEBUGBAR****************************************************
+use DebugBar\StandardDebugBar;
 $debugbar = new StandardDebugBar();
 $debugbarRenderer = $debugbar->getJavascriptRenderer();
 
 $debugbar["messages"]->addMessage("hello world!");
+
+
+
+// ROUTER****************************************************
+// Create Router instance
+$router = new \Bramus\Router\Router();
+// Custom 404 Handler
+$router->set404(function () {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+    echo '404, route not found!';
+});
+
+// Static route: / (homepage)
+$router->get('/', function () {
+    echo 'Homepage';
+});
+
+
+// Run it!
+$router->run();
+
 ?>
 
 
+
+<!-- HTML ****************************************************** -->
 <html>
 <head>
     <?php echo $debugbarRenderer->renderHead() ?>
